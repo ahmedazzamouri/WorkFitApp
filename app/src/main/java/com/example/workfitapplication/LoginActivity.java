d62fd24d5914b4746aac31bdf15e8a8389a09ee8
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2022-04-11 17:27:07 +0200

    Changed some names files

--- app/src/main/java/com/example/workfitapplication/LoginActivity.java
+++ app/src/main/java/com/example/workfitapplication/LoginActivity.java
@@ -34,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
