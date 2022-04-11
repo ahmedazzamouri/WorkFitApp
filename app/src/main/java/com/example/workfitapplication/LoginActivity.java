53b1ab41f5fd882bc632fc85018bcc89c0642c00
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2022-04-11 14:33:43 +0200

    Login activity modified

--- app/src/main/java/com/example/workfitapplication/LoginActivity.java
+++ app/src/main/java/com/example/workfitapplication/LoginActivity.java
@@ -1,7 +1,78 @@
 package com.example.workfitapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText email, password;
+    Button login;
+    ImageButton register;
+    boolean isEmailValid, isPasswordValid;
+    TextInputLayout emailError, passError;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email=findViewById(R.id.login_email);
+        password=findViewById(R.id.login_pwd);
+        login = findViewById(R.id.login_bttn);
+        register = findViewById(R.id.register_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SetValidation();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public void SetValidation(){
+        if (email.getText().toString().isEmpty()){
+            emailError.setError(getResources().getString(R.string.email_error));
+            isEmailValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()){
+            emailError.setError(getResources().getString(R.string.error_invalid_email));
+            isEmailValid = false;
+        } else {
+            isEmailValid = true;
+            emailError.setErrorEnabled(false);
+        }
+
+        if (password.getText().toString().isEmpty()){
+            passError.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (password.getText().length() < 6){
+            passError.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else {
+            isPasswordValid = true;
+            passError.setErrorEnabled(false);
+        }
+
+        if (isEmailValid && isPasswordValid){
+            Toast.makeText(getApplicationContext(),"Login successfully!", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
