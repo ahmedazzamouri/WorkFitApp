dfc8dddc712e0d5888e90c6e572cc081ca58c8c2
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2022-04-11 17:20:21 +0200

    Changed some names files

--- app/src/main/java/com/example/workfitapplication/MainActivity.java
+++ app/src/main/java/com/example/workfitapplication/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.workfitapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    Toolbar toolbar;
+    NavigationView nv;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nv = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        nv.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_timer){
+            Intent intent1 = new Intent(MainActivity.this, Timer.class);
+            startActivity(intent1);
+        }
+        else if (id == R.id.nav_profile){
+            //Intent intent2 = new Intent(MainActivity.this, UserProfile.class);
+            //startActivity(intent2);
+        }
+        else if (id == R.id.nav_splits){
+            Intent intent3 = new Intent(MainActivity.this, DaySplits.class);
+            startActivity(intent3);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
