84bda977376886e7eedc550a605a00d396ad8983
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2022-04-13 15:36:54 +0200

    StepCountActivity modified v2

--- app/src/main/java/com/example/workfitapplication/StepCountActivity.java
+++ app/src/main/java/com/example/workfitapplication/StepCountActivity.java
@@ -1,16 +1,21 @@
 package com.example.workfitapplication;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class StepCountActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -23,11 +28,20 @@ public class StepCountActivity extends AppCompatActivity implements SensorEventL
     int stepCount = 0;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steps);
 
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION},0);
+                }
+            }
+        }
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         tv_StepCounter= findViewById(R.id.steps);
         tv_StepDetector = findViewById(R.id.steps_detect);
